e834395d75a138ce95cdf9bdac8f270e7d006c10
NitritoM <aressz@gmail.com>
2021-12-26 16:28:43 -0500

    PrimeraEntregaParcialReact

--- src/components/ItemDetailContainer.js
+++ src/components/ItemDetailContainer.js
@@ -1,25 +1,78 @@
 import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import ItemDetail from "./ItemDetail";
+
 
 const ItemDetailContainer = (props) => {
 	const [state, setState] = useState([]);
+	const { Id } = useParams();
+	console.log(Id);
 
 	const getData = new Promise((resolve, reject) => {
 		setTimeout(() => {
-			resolve([{ id: 1, name: "Bailarina de Marinera", project: "Las mil danzas de Peru", place: "San Lazaro - Arequipa", img: "img/ASFWP (1).jpg", price: "120$" }]);
-		}, 2000);
+			switch (Id) {
+				case "1":
+					resolve([
+						{
+							id: 1,
+							name: "Bailarina de Marinera",
+							project: "Danza",
+							place: "San Lazaro - Arequipa",
+							img: "../img/ASFWP (1).jpg",
+							price: "120$",
+						},
+					]);
+					break;
+				case "2":
+					resolve([
+						{
+							id: 2,
+							name: "Moreno",
+							project: "Vejez",
+							place: "San Lazaro - Arequipa",
+							img: "../img/ASFWP (2).jpg",
+							price: "120$",
+						},
+					]);
+					break;
+				case "3":
+					resolve([
+						{
+							id: 3,
+							name: "Bailarina de Marinera",
+							project: "Belleza",
+							place: "San Lazaro - Arequipa",
+							img: "../img/ASFWP (3).jpg",
+							price: "120$",
+						},
+					]);
+					break;
+
+				default:
+					resolve([
+						{
+							id: 1,
+							name: "Bailarina de Marinera",
+							project: "Las mil danzas de Peru",
+							place: "San Lazaro - Arequipa",
+							img: "../img/ASFWP (1).jpg",
+							price: "120$",
+						},
+					]);
+					break;
+			}
+		}, 500);
 	});
 
 	useEffect(() => {
 		getData.then((res) => setState(res));
-	}, []);
+	}, [Id]);
 
 	return (
 		<>
 			<div className="row justify-content-center">
 				<div className="col-md-4 info__product-name justify-content-center">
 					{state.map((value) => {
-						console.log(value.img);
 						return <ItemDetail key={value.id} name={value.name} project={value.project} place={value.place} img={value.img} price={value.price} />;
 					})}
 				</div>
