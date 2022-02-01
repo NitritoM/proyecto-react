a2ae5804daa5fbade1103ed491c4ee90272f18db
NitritoM <aressz@gmail.com>
2022-01-31 21:33:25 -0500

    Commit Final

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,8 +9,7 @@ function Home() {
 			</div>
 			<div>
 				<p>Esta pagina es una pagina para un fotografo, el cual desea vender los prints de las fotografias mas reconocidas de sus proyectos</p>
-				<h2>IMPORTANTE: ya termine lo referente a los desafios que me faltaban los que involucran a firebase</h2>
-				<h2>IMPORTANTE2: los items que me faltan corregir (agregar botones y demas) los pondre para la ultima entrega ya no creo que me alcance para hacerlo ahora</h2>
+				<h2>IMPORTANTE2: los items que me faltan corregir (agregar botones y demas) </h2>
 				<h2>IMPORTANTE3: MUCHAS GRACIAS POR TODO !!!!</h2>
 				<h2>ME FALTA PULIR Y LIMPIAR EL CODIGO DE LAS COSAS QUE NO USO</h2>
 
